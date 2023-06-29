ff71410e264d91f3a1df5faad2ca5d05c32b9252
= <ronaldo.alves.rios@gmail.com>
2023-06-28 21:21:57 -0300

    novo

--- src/br/com/banco/business/ProcessoAgendamentoDeposito.java
+++ src/br/com/banco/business/ProcessoAgendamentoDeposito.java
@@ -1,5 +1,6 @@
 package br.com.banco.business;
 
+import br.com.banco.model.Cliente;
 import br.com.banco.util.FileUtils;
 
 public class ProcessoAgendamentoDeposito {
@@ -8,6 +9,19 @@ public class ProcessoAgendamentoDeposito {
 		try {
 			StringBuffer conteudo  = FileUtils.readFile("database/", "processo_deposito.txt");
 			String[] array = conteudo.toString().split(",");
+			
+			
+			
+			for(String e : array) {
+				
+				Cliente c = new Cliente();
+				c.setId(array[0]);
+				c.setConta(null);
+				c.setCpf(null);
+				
+			}
+			
+			
 			System.out.println(conteudo);
 		} catch (Exception e) {
 			throw new Exception("Não foi possível ler o arquivo.");
